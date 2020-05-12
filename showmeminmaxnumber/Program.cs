ed57a95d19cac97b981b4c285c6e847219e229a9
dayeon1021 <s2018s20@e-mirim.hs.kr>
2020-05-12 10:42:24 +0900

    예제 7번 실습 큰 수, 작은 수 찾기

--- showmeminmaxnumber/Program.cs
+++ showmeminmaxnumber/Program.cs
@@ -10,6 +10,30 @@ namespace showmeminmaxnumber
     {
         static void Main(string[] args)
         {
+            int[] array = new int[5];
+            int temp;
+
+            for(int i = 0; i < 5; i++)
+            {
+                Console.Write("숫자를 입력해주세요 : ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            
+            for(int i = 0; i <array.Length; i++)
+            {
+                for(int j = i+1; j<array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("가장 큰 수 : " + array[4]);
+            Console.WriteLine("가장 작은 수 : " + array[0]);
         }
     }
 }
